734d7b48f8fc7c4da67a2d1e7894b094a98d3e12
Unknown <sergeitchaban@gmail.com>
2019-04-23 18:35:29 +0300

    Added new icon sizes for manifest

--- precache-manifest.bab9e792bd0276d11d56e1d57ab2e682.js
+++ precache-manifest.bab9e792bd0276d11d56e1d57ab2e682.js
@@ -1,87 +1,91 @@
 self.__precacheManifest = [
   {
-    "revision": "e0fc4e5c719b4dc10c97fc111d7374e1",
-    "url": "/static/media/fa-brands-400.e0fc4e5c.ttf"
+    "revision": "5a4618f029618cc2795c05fe53d57028",
+    "url": "/static/media/fa-regular-400.5a4618f0.eot"
   },
   {
-    "revision": "bb2a63cbb251bb3f7366",
+    "revision": "b0807f430013a8bab046",
     "url": "/static/css/main.9a47b17a.chunk.css"
   },
   {
-    "revision": "d4c5375f230aec975d78",
-    "url": "/static/js/1.d4c5375f.chunk.js"
+    "revision": "1da42f622713e83582ed",
+    "url": "/static/js/1.1da42f62.chunk.js"
   },
   {
-    "revision": "3835369eeb88ae5cbf10",
-    "url": "/static/js/2.3835369e.chunk.js"
+    "revision": "8a9deb0321ba1b472192",
+    "url": "/static/js/2.8a9deb03.chunk.js"
   },
   {
-    "revision": "30c3dad5395fc6f4113c",
-    "url": "/static/js/3.30c3dad5.chunk.js"
+    "revision": "6267890a8373572a3303",
+    "url": "/static/js/3.6267890a.chunk.js"
   },
   {
-    "revision": "21666d780a9c29fa5a53",
-    "url": "/static/js/4.21666d78.chunk.js"
+    "revision": "10f4c544d8e885fd30e1",
+    "url": "/static/js/4.10f4c544.chunk.js"
   },
   {
-    "revision": "e5454ac4b727eb686f39",
-    "url": "/static/js/5.e5454ac4.chunk.js"
+    "revision": "7e0783f44ba615f08059",
+    "url": "/static/js/5.7e0783f4.chunk.js"
   },
   {
-    "revision": "3080fe5021a4d0af89a2",
-    "url": "/static/js/6.3080fe50.chunk.js"
+    "revision": "a58fb8862b4edd3c307a",
+    "url": "/static/js/6.a58fb886.chunk.js"
   },
   {
-    "revision": "f2d45dcddef2c73da756",
-    "url": "/static/js/7.f2d45dcd.chunk.js"
+    "revision": "5ac8e24297a1b38b3bbe",
+    "url": "/static/js/7.5ac8e242.chunk.js"
   },
   {
     "revision": "1c3f67aa7a72202f7a560abafbacf909",
     "url": "/static/media/screen_4.1c3f67aa.png"
   },
   {
-    "revision": "5199aa78e26aea40463f",
-    "url": "/static/js/8.5199aa78.chunk.js"
+    "revision": "d830d5352fe1b277763c",
+    "url": "/static/js/8.d830d535.chunk.js"
   },
   {
-    "revision": "79c8dca9a6a5bec0adfe",
-    "url": "/static/js/9.79c8dca9.chunk.js"
+    "revision": "6d3205f6842f431700b5",
+    "url": "/static/js/9.6d3205f6.chunk.js"
   },
   {
-    "revision": "f6bb0d5c5c417efcf56f",
-    "url": "/static/js/10.f6bb0d5c.chunk.js"
+    "revision": "f14b99aa6bff9b8281cf",
+    "url": "/static/js/10.f14b99aa.chunk.js"
   },
   {
     "revision": "7516b305790514c283a1",
     "url": "/static/js/11.7516b305.chunk.js"
   },
   {
-    "revision": "9a5fdff22dbd5652650e",
-    "url": "/static/js/runtime~main.9a5fdff2.js"
+    "revision": "1d89ef3cfdff5ecdeddf",
+    "url": "/static/js/runtime~main.1d89ef3c.js"
   },
   {
     "revision": "ac8645313cb3df483d70f7d0d9090b72",
     "url": "/static/media/Whitney-Medium.ac864531.ttf"
   },
   {
-    "revision": "f319eac1c755f9929fd856720ce1695e",
-    "url": "/static/media/fa-brands-400.f319eac1.woff2"
-  },
-  {
-    "revision": "d034c1b2ee84dd981ef2e637754a0b4f",
-    "url": "/static/media/fa-brands-400.d034c1b2.woff"
-  },
-  {
     "revision": "e2a76403183eff8967cf6318c6e51509",
     "url": "/static/media/fa-brands-400.e2a76403.eot"
   },
   {
-    "revision": "bb2a63cbb251bb3f7366",
-    "url": "/static/js/main.bb2a63cb.chunk.js"
+    "revision": "f319eac1c755f9929fd856720ce1695e",
+    "url": "/static/media/fa-brands-400.f319eac1.woff2"
   },
   {
     "revision": "a3715c6fe264a51f1d9260b447ff46bc",
     "url": "/static/media/fa-regular-400.a3715c6f.woff2"
+  },
+  {
+    "revision": "b0807f430013a8bab046",
+    "url": "/static/js/main.b0807f43.chunk.js"
+  },
+  {
+    "revision": "e0fc4e5c719b4dc10c97fc111d7374e1",
+    "url": "/static/media/fa-brands-400.e0fc4e5c.ttf"
+  },
+  {
+    "revision": "d034c1b2ee84dd981ef2e637754a0b4f",
+    "url": "/static/media/fa-brands-400.d034c1b2.woff"
   },
   {
     "revision": "e99569d3d10c94c60d9a68523c1c0e71",
@@ -96,20 +100,12 @@ self.__precacheManifest = [
     "url": "/static/media/fa-regular-400.e9d8dbb0.svg"
   },
   {
-    "revision": "5a4618f029618cc2795c05fe53d57028",
-    "url": "/static/media/fa-regular-400.5a4618f0.eot"
-  },
-  {
-    "revision": "d9824d00712532d7697df68df16ae0d3",
-    "url": "/static/media/fa-solid-900.d9824d00.eot"
-  },
-  {
     "revision": "18d2347ab2a9f40ca2247cdb03303d84",
     "url": "/static/media/fa-solid-900.18d2347a.woff2"
   },
   {
-    "revision": "a999c7b8823b901248833dc7fe07a52f",
-    "url": "/static/media/fa-brands-400.a999c7b8.svg"
+    "revision": "d9824d00712532d7697df68df16ae0d3",
+    "url": "/static/media/fa-solid-900.d9824d00.eot"
   },
   {
     "revision": "128d2b0be23925e5cf36717ddc6f093b",
@@ -118,6 +114,10 @@ self.__precacheManifest = [
   {
     "revision": "00ddaede094b83270cadbfc1a907e8ca",
     "url": "/static/media/fa-solid-900.00ddaede.ttf"
+  },
+  {
+    "revision": "a999c7b8823b901248833dc7fe07a52f",
+    "url": "/static/media/fa-brands-400.a999c7b8.svg"
   },
   {
     "revision": "666a82cb3e9f8591bef4049aea26c4c6",
@@ -148,11 +148,11 @@ self.__precacheManifest = [
     "url": "/static/media/screen_3.12de9de1.png"
   },
   {
-    "revision": "5199aa78e26aea40463f",
-    "url": "/static/css/8.9a1adf5d.chunk.css"
+    "revision": "d830d5352fe1b277763c",
+    "url": "/static/css/8.83477773.chunk.css"
   },
   {
-    "revision": "0a7d24c7ba10c517ef5b61f3c734a49e",
+    "revision": "ad08ec08c3673c5fdd96379f2a528ce1",
     "url": "/index.html"
   }
 ];
